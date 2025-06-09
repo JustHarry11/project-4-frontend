6e285a3e62defa379a2f67b0f0166d0fddda93fd
Harry <harrylippa@gmail.com>
2025-06-09 10:23:25 +0100

    Created a show page which shows all the details about the boardgame

--- src/components/BoardgameShow/BoardgameShow.jsx
+++ src/components/BoardgameShow/BoardgameShow.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../hooks/useFetch"
 import { useContext } from "react"
 import { UserContext } from "../../contexts/UserContext"
 import { Link } from "react-router"
-
+import './BoardgameShow.css'
 import BoardgameDelete from "../BoardgameDelete/BoardgameDelete"
 
 export default function BoardgameShow() {
@@ -27,15 +27,39 @@ export default function BoardgameShow() {
                 : isLoading
                     ? <p>Loading...</p>
                     : (
-                        <section>
-                            <h1>{boardgame.title}</h1>
-                            <h2>{boardgame.description}</h2>
-                            {user && user.id === boardgame.owner?.id &&
-                                <div className="controls">
-                                    <Link className="edit-boardgame" to={`/boardgames/${boardgameId}/edit`}>Edit</Link>
-                                    <BoardgameDelete />
-                                </div>}
-                        </section>
+                        <div className="single-boardgame-container">
+                            <div className="single-boardgame">
+                                <h1>{boardgame.title}</h1>
+                                <img src={boardgame.image_url} alt={boardgame.title} className='boardgame-image' />
+                                <div className="single-content">
+                                    <div className='single-type-genre'>
+                                        <span>Type - {boardgame.type}</span>
+                                        <span>Genre - {boardgame.genre}</span>
+                                    </div>
+                                    <div className='single-min-max-players'>
+                                        <span>Max Players - {boardgame.max_players}</span>
+                                        <span>Min Players - {boardgame.min_players}</span>
+                                    </div>
+                                    <div className='single-likes'>
+                                        <span>Likes {boardgame.likes.length}</span>
+                                    </div>
+                                </div>
+
+                                <div className="single-boardgame-content">
+                                    <p>{boardgame.description}</p>
+                                    <p>{boardgame.instruction}</p>
+                                </div>
+                                <div className='owner'>
+                                    <span>{boardgame.owner.username.charAt(0).toUpperCase() + boardgame.owner.username.slice(1)}</span>
+                                </div>
+                                {user && user.id === boardgame.owner?.id &&
+                                    <div className="single-control">
+                                        <Link className="single-edit-boardgame" to={`/boardgames/${boardgameId}/edit`}>Edit</Link>
+                                        <BoardgameDelete />
+                                    </div>}
+                            </div>
+                        </div>
+
                     )}
 
         </>
